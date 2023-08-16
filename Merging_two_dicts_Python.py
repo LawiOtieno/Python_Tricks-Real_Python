a11152534522d46740757ddbabb1d68d7f159217
LAWRENCE OTIENO <laurielawi12@gmail.com>
2023-08-16 12:42:06 +0300

    Merging two dicts in Python

---
+++ Merging_two_dicts_Python.py
@@ -0,0 +1,7 @@
+# How to print only 3 names of a person
+
+name_1 = {'first': "Lawi", 'second': "Olel"}
+name_2 = {'second': "Odhiambo", 'third': "Otieno"}
+
+only_3 = {**name_1, **name_2}
+print(only_3)
